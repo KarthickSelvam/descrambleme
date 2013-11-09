f50391ed23690c3e42eb54f1311b106cf8c22ef0
VigneshPT <vignesh294@gmail.com>
2013-11-09 14:46:07 +0530

    refactored code in descriptionJson, and indented code in gameplay.js

--- public/js/gameplay.js
+++ public/js/gameplay.js
@@ -1,112 +1,111 @@
-String.prototype.shuffle = function () {
-		var a = this.split(""),
+String.prototype.shuffle = function() {
+	var a = this.split(""),
 		n = a.length;
-		for(var i = n - 1; i > 0; i--) {
+	for (var i = n - 1; i > 0; i--) {
 		var j = Math.floor(Math.random() * (i + 1));
 		var tmp = a[i];
 		a[i] = a[j];
 		a[j] = tmp;
-		}
-		return a.join("");
-		}
-		var myCountdown2 = new Countdown({
-									time: 30, 
-									width:200, 
-									height:80, 
-									rangeHi:"minute",	// <- no comma on last item!
-									onComplete	: countdownComplete
-									});
-									
-		
-		var scrambledWord = originalWord.shuffle()+"aeiou".shuffle();
-		var wordLength = originalWord.length;
-		var clickCount = -1;
-		function countdownComplete()
-		{
-			alert(" Challenge Lost. Click Ok to Learn the Word !");
-			document.location = "/description?word="+originalWord;
-		}
-		function createElements(str) {	
-		
-			for(i=0;i<str.length;i++)
-			{	
-			var classType=['floating','floatingr','scaling'];		
-			var rand = Math.floor(Math.random()*2);			
-			
-			var animation=document.createElement('div');
-			$(animation).append(str[i]).addClass(classType[rand])
-			
-			var bubbles = document.createElement('div');
-			$(bubbles).addClass('bubble x'+((i+1)%14)+' ball ').append(animation)
-			
-			$("#bubble").append(bubbles);
-			}
+	}
+	return a.join("");
+}
+var myCountdown2 = new Countdown({
+	time: 30,
+	width: 200,
+	height: 80,
+	rangeHi: "minute", // <- no comma on last item!
+	onComplete: countdownComplete
+});
 
-			for(j=0;j<wordLength;j++)
-			{
-			var blanks=document.createElement('div');
-			$(blanks).append('<h1></h1>').addClass('blank').attr('id', 'blank'+j)
-			
-			$("#userAnswer").append(blanks);			
-			}
 
-		}
-		$(document).ready(function(){
-		
-		var audioElement = document.createElement('audio');
-		audioElement.setAttribute('src', '/audio/Minion_What.mp3');		
-		
-		
-		createElements(scrambledWord);
-		
-		$('.bubble').on('click', function () {
+var scrambledWord = originalWord.shuffle() + "aeiou".shuffle();
+var wordLength = originalWord.length;
+var clickCount = -1;
+
+function countdownComplete() {
+	alert(" Challenge Lost. Click Ok to Learn the Word !");
+	document.location = "/description?word=" + originalWord;
+}
+
+function createElements(str) {
+
+	for (i = 0; i < str.length; i++) {
+		var classType = ['floating', 'floatingr', 'scaling'];
+		var rand = Math.floor(Math.random() * 2);
+
+		var animation = document.createElement('div');
+		$(animation).append(str[i]).addClass(classType[rand])
+
+		var bubbles = document.createElement('div');
+		$(bubbles).addClass('bubble x' + ((i + 1) % 14) + ' ball ').append(animation)
+
+		$("#bubble").append(bubbles);
+	}
+
+	for (j = 0; j < wordLength; j++) {
+		var blanks = document.createElement('div');
+		$(blanks).append('<h1></h1>').addClass('blank').attr('id', 'blank' + j)
+
+		$("#userAnswer").append(blanks);
+	}
+
+}
+$(document).ready(function() {
+
+	var audioElement = document.createElement('audio');
+	audioElement.setAttribute('src', '/audio/Minion_What.mp3');
+
+
+	createElements(scrambledWord);
+
+	$('.bubble').on('click', function() {
 		clickCount++;
 		var clickedLetter = $(this).find('div').html();
-		if(originalWord[clickCount] == clickedLetter){
-		
-				$('#userAnswer').find('#blank'+clickCount+' h1').html(clickedLetter);
-				if(clickCount+1==wordLength)
-					{
-						alert('Congrats ! You have Passed the Challenge . Click ok! ');
-						document.location = "/description?word="+originalWord;
-					}
+		if (originalWord[clickCount] == clickedLetter) {
+
+			$('#userAnswer').find('#blank' + clickCount + ' h1').html(clickedLetter);
+			if (clickCount + 1 == wordLength) {
+				alert('Congrats ! You have Passed the Challenge . Click ok! ');
+				document.location = "/description?word=" + originalWord;
+			}
+		} else {
+			clickCount--;
+			audioElement.play();
 		}
-		else
-		{
-		clickCount--;
-		audioElement.play();
+	});
+
+	var globalResult;
+
+	$.ajax({
+		url: '/descriptionJson?word=' + originalWord,
+		dataType: 'json',
+		success: function(result) {
+			globalResult = result;
+			$("#defni").append(result.Definition);
+			var urls = result.Images.split(',');
+			//audio file
+			var audio = document.createElement('audio');
+			$(audio).attr('src', 'http://mediaapps.globalenglish.com/audio/tts.php?voice=0&speed=1&text=' + globalResult.KeyWord).attr('controls', 'controls')
+			$("#audioSpan").append(audio);
+
+			for (i = 0; i < 1; i++) {
+				var images = document.createElement('img');
+				$(images).addClass('hintImage').attr('src', urls[i])
+				$("#imageSpan").append(images);
+			}
 		}
-		});
-		
-		var globalResult;
+	});
 
-		$.ajax({url:'/descriptionJson?word='+originalWord,dataType:'json',success:function(result){
-		globalResult=result;
-		$("#defni").append(result.Definition);  
-		var urls=result.Images.split(',');
-		//audio file
-		var audio=document.createElement('audio');
-		$(audio).attr('src','http://mediaapps.globalenglish.com/audio/tts.php?voice=0&speed=1&text='+globalResult.KeyWord).attr('controls','controls')
-		$("#audioSpan").append(audio);
+	$('#audioHint').click(function() {
+		$('#audiospan').toggle('slow')
+	});
+	$('#imageHint').on('click', this, function() {
 
-		for(i=0;i<1;i++)
-		{ var images=document.createElement('img');
-		$(images).addClass('hintImage').attr('src', urls[i])   
-		$("#imageSpan").append(images);
-		} 
-		}});
+		$('#hint').find('#imageSpan').removeClass('hide');
+	});
+	$('#audioHint').on('click', this, function() {
 
-		$('#audioHint').click(function() {
-		$('#audiospan').toggle('slow')  
-		}); 
-		$('#imageHint').on('click',this,function(){
-			
-			$('#hint').find('#imageSpan').removeClass('hide');
-		});
-		$('#audioHint').on('click',this,function(){
-			
-			$('#hint').find('#audioSpan').removeClass('hide');
-		});
-		
-		});
-		
+		$('#hint').find('#audioSpan').removeClass('hide');
+	});
+
+});
--- viewcontrollers/descriptionJson.js
+++ viewcontrollers/descriptionJson.js
@@ -1,261 +1,146 @@
-var http = require("http");
-var wordlist = "";
-var imagecollection = "";
-var keyWord = "";
-var Phrase = "";
-var Anagrams = "";
-var Images = "";
+var request = require('request');
 var parseString = require('xml2js').parseString;
 
 
-module.exports.controller = function(app){
- app.get('/descriptionJson',function(req,res){
- getWordDetails(res, req.query.word);
- });
-};
 
+module.exports.controller = function(app) {
+    app.get('/descriptionJson', function(req, res) {
+        /*Variables declaration starts */
+        var wordDetails = {};
+        var imageList = [];
+        var phrasesList = {};
+        var anagramsString = "";
 
-function getWordDetails(res,value) 
-{                var http = require("http");
-                var options = {
-                        host : 'api.pearson.com',
-                        path : '/v2/dictionaries/entries?headword='+value+'&apikey=6pUM7idZK2khzpx31xSfUoUapA2wQbzm',
-                        port : 80,
-                        method : 'GET'
+        var Phrase = "";
+        var Anagrams = "";
+        var Images = [];
+        var Speech = "",
+            Definition = "",
+        Usage = "",
+        Audio = "",
+        Synonyms = "",
+        Antonyms = "";
+        /*Variables ends */
+
+        var inputWord = req.query.word;
+
+        /*function definitions */
+        var getWordDetails = function(keyWord) {
+            url = 'http://api.pearson.com/v2/dictionaries/entries?headword=' + keyWord + '&apikey=6pUM7idZK2khzpx31xSfUoUapA2wQbzm';
+            request(url, function(err, response, data) {
+                console.log(err);
+                if (!err && response.statusCode == 200) {
+                    wordDetails = JSON.parse(data);
+                    console.log(wordDetails);
+
+                    if (Object.keys(wordDetails).length > 0) {
+                        for (var item = 0; item < wordDetails.results.length; item++) {
+                            if (wordDetails.results[item].headword == keyWord) {
+                                Definition = wordDetails.results[item].senses[0].definition;
+                                if (wordDetails.results[item].senses[0].examples) {
+                                    Usage = wordDetails.results[item].senses[0].examples[0].text;
+                                    if (wordDetails.results[item].senses[0].examples[0].audio)
+                                        Audio = "http://api.pearson.com" + wordDetails.results[item].senses[0].examples[0].audio[0].url;
+                                }
+                                break;
+                            }
+                        }
+                    }
                 }
-                var request = http.request(options, function(response){
-                        var body = ""
-                        response.on('data', function(data) {
-                          body += data;
-                          wordlist = body;
-                        });
-                        response.on('end', function() {        
-                                wordlist = body;
-                                getDKImages(value,res,JSON.parse(body));
-                        });
-                });
-                request.on('error', function(e) {
-                console.log('Problem with request: ' + e.message);
-                });                                
-                request.end();
-}
+                getDKImages(keyWord);
+            });
+        }
+        var getDKImages = function(keyWord) {
+            url = 'http://api.pearson.com/dk/v1/images?caption=' + keyWord + '&limit=3&apikey=6pUM7idZK2khzpx31xSfUoUapA2wQbzm';
+            request(url, function(err, response, data) {
 
-function getDKImages(value,res,obj) 
-{                var http = require("http");
-                var options = {
-                        host : 'api.pearson.com',
-                        path : '/dk/v1/images?caption='+value+'&limit=3&apikey=6pUM7idZK2khzpx31xSfUoUapA2wQbzm',
-                        port : 80,
-                        method : 'GET'
+                if (!err && response.statusCode == 200) {
+                    imageList = JSON.parse(data);
+
+                    if (imageList.images && imageList.images.length > 0) {
+                        imageList.images.forEach(function(imageItem) {
+                            Images.push(imageItem.url);
+                        })
+                        
+                    }
+                    else
+                        getGoogleImages(keyWord);
                 }
-                var request = http.request(options, function(response){
-                        var body = ""
-                        response.on('data', function(data) {
-                          body += data;
-                        });
-                        response.on('end', function() {        
-                                imagecollection = JSON.parse(body);
-                                if(imagecollection.images.length > 0)
-                                        getPhrases(value,res,imagecollection,obj);
-                                else
-                                        getGoogleImages(value,res,obj)
-                                        
-                        });
-                });
-                request.on('error', function(e) {
-                console.log('Problem with request: ' + e.message);
-                });
-                request.end();
-}
-
-function getGoogleImages(value,res,obj) 
-{                var http = require("http");
-                var options = {
-                        host : 'ajax.googleapis.com',
-                        path : '/ajax/services/search/images?v=1.0&q='+value,
-                        port : 80,
-                        method : 'GET'
-                }
-                var request = http.request(options, function(response){
-                        var body = ""
-                        response.on('data', function(data) {
-                          body += data;
-                        });
-                        response.on('end', function() {        
-                                imagecollection = JSON.parse(body);
-                                //console.log('Problem with request: ' + JSON.stringify(imagecollection));
-                                getPhrases(value,res,imagecollection,obj);
-                        });
-                });
-                request.on('error', function(e) {
-                console.log('Problem with request: ' + e.message);
-                });
-                request.end();
-}
-
-
-function getPhrases(value,res,imagecollection,obj) 
-{                var http = require("http");
-                var options = {
-                        host : 'stands4.com',
-                        path : '/services/v2/phrases.php?uid=2167&tokenid=LL870U6UWvQIh9w&phrase='+value,
-                        port : 80,
-                        method : 'GET'
-                }
-                var request = http.get(options, function(response){
-                        var body = ""
-                        response.on('data', function(data) {
-                          body += data;
+                getPhrases(keyWord);
+            });
+        }
+        var getGoogleImages = function(keyWord) {
+            url = 'http://ajax.googleapis.com/ajax/services/search/images?v=1.0&q=' + keyWord;
+            request(url, function(err, response, data) {
+                if (!err && response.statusCode == 200) {
+                    imageList = JSON.parse(data);
+                    if (imageList.responseData && imageList.responseData.results) {
+                        imageList.responseData.results.forEach(function(result) {
+                            Images.push(result.url);
                         });
                         
-                        response.on('end', function() {
-                                var phraseObj = "";
-                                parseString(body, function (err, result) {
-                                        phraseObj = result;        
-                                });
-                                getAnagrams(value,res,imagecollection,obj,phraseObj)                                
-                        });
-                });
-                request.on('error', function(e) {
-                        console.log('Problem with request: ' + e.message);
-                });
-                request.end();
-}
-
-function getAnagrams(value,res,imagecollection,obj,phraseObj)
-{
-        var http = require("http");
-                var options = {
-                        host : 'anagramica.com',
-                        path : '/best/:'+value,
-                        port : 80,
-                        method : 'GET'
+                    }
                 }
-                var request = http.get(options, function(response){
-                        var body = ""
-                        response.on('data', function(data) {
-                          body += data;
-                        });
-                        
-                        response.on('end', function() {
-                                var data= JSON.parse(body);
-                                var letter = "";
-                                for (var l = 0; l < data.best.length; l++) {
-                                        if(l == data.best.length-1)
-                                                letter  = letter + data.best[l];
-                                                else
-                                                letter  = letter + data.best[l] + ","
-                                }
-                                getSynonyms(value,res,imagecollection,obj,phraseObj,letter)                                
-                        });
-                });
-                request.on('error', function(e) {
-                        console.log('Problem with request: ' + e.message);
-                });
-                request.end();
-}
+            });
+        };
+        var getPhrases = function(keyWord) {
+            url = 'http://stands4.com/services/v2/phrases.php?uid=2167&tokenid=LL870U6UWvQIh9w&phrase=' + keyWord;
+            request(url, function(err, response, data) {
+                if (!err && response.statusCode == 200) {
+                    parseString(data, function(error, result) {
+                        phraseObj = result;
+                        if (!error && phraseObj.results.result) {
+                            Phrase = phraseObj.results.result[0].explanation;
+                        } else if (error) {
+                            console.log('parse error in getPhrases: ' + error.message);
+                        }
 
-function getSynonyms(value,res,imagecollection,obj,phraseObj,letter)
-{        var Definition = "";
-        var Audio = "";
-        var Usage = "";
-        var http = require("http");
-                var options = {
-                        host : 'stands4.com',
-                        path : '/services/v2/syno.php?uid=2167&tokenid=LL870U6UWvQIh9w&word='+value,
-                        port : 80,
-                        method : 'GET'
+                    });
                 }
-                var request = http.get(options, function(response){
-                        var body = ""
-                        response.on('data', function(data) {
-                          body += data;
-                        });
-                        
-                        response.on('end', function() {
-                                var synonymObj = "";
-                                parseString(body, function (err, result) {
-                                        synonymObj = result;        
-                                });
-                                if(Object.keys(obj).length > 0)
-                                {                                                
-                                        for (var item = 0; item < obj.results.length; item++) {
-                                                        if(obj.results[item].headword == value)
-                                                        {        
-                                                                keyWord = obj.results[item].headword;
-                                                                Definition = obj.results[item].senses[0].definition;
-                                                                //console.log('Problem with request: ' + Definition);
-                                                                if(obj.results[item].senses[0].examples)
-                                                                {                                                
-                                                                Usage = obj.results[item].senses[0].examples[0].text;
-                                                                if(obj.results[item].senses[0].examples[0].audio)
-                                                                Audio = "http://api.pearson.com"+obj.results[item].senses[0].examples[0].audio[0].url;
-                                                                }
-                                                                item = obj.results.length +1;
-                                                        }
-                                        }
-                                }
-                                //console.log('Problem with request: ' + JSON.stringify(phraseObj));
-                                if(phraseObj.results.result){
-                                        Phrase = phraseObj.results.result[0].explanation
-                                }                        
-                                var image = "";
-                                //console.log('Problem with request: ' + JSON.stringify(imagecollection));
-                                //console.log('Problem with request: ' + JSON.stringify(imagecollection));
-                                if(imagecollection.images)
-                                {        //console.log('if: ' + JSON.stringify(imagecollection));
-                                        if(imagecollection.images.length > 0)
-                                        {
-                                                for (var l = 0; l < imagecollection.images.length; l++) {
-                                                        if(l == imagecollection.images.length-1)
-                                                        image  = image + imagecollection.images[l].url;
-                                                        else
-                                                        image  = image + imagecollection.images[l].url + ","
-                                                }
-                                        }
-                                }
-                                else
-                                {        if(imagecollection.responseData.results)
-                                        {
-                                                for (var l = 0; l < imagecollection.responseData.results.length; l++) 
-                                                {        if(l == imagecollection.responseData.results.length-1)
-                                                        image  = image + imagecollection.responseData.results[l].url;
-                                                        else
-                                                        image  = image + imagecollection.responseData.results[l].url + ","
-                                                }
-                                        }
-                                }
-                                
-                                var Synonyms = "";
-                                var Antonyms = "";
-                                var speech = "";
-                                if(synonymObj.results.result)
-                                {        
-                                                Synonyms = synonymObj.results.result[0].synonyms;
-                                                Antonyms = synonymObj.results.result[0].antonyms;
-                                                speech = synonymObj.results.result[0].partofspeech;
-                                                if(Definition == "" || Definition == null)
-                                                        Definition = synonymObj.results.result[0].definition;
-                                }
-                                //console.log('Problem with request: ' + Definition + synonymObj.results.result[0].definition);
-                                
-                                var collection = {'KeyWord':value,'Speech':speech,'Audio':Audio,'Definition':Definition,'Usage':Usage,'Phrase':Phrase,'Synonyms':Synonyms,'Antonyms':Antonyms,'Anagrams':letter,'Images':image}
-                                
-                                res.send(collection)                                
-                                
-                        });
-                });
-                request.on('error', function(e) {
-                        console.log('Problem with request: ' + e.message);
-                });
-                request.end();
-}
+                getAnagrams(keyWord);
+            });
+        };
+        var getAnagrams = function(keyWord) {
+            url = 'http://anagramica.com/best/:' + keyWord;
+            request(url, function(err, response, anagramdata) {
+                if (!err && response.statusCode == 200) {
+                    var jsonData = JSON.parse(anagramdata);
+                    console.log(jsonData);
+                    Anagrams = jsonData['best'].join(",");
+                }
+                getSynonyms(keyWord);
+            });
+        };
+        var getSynonyms = function(keyWord) {
+            url = 'http://stands4.com/services/v2/syno.php?uid=2167&tokenid=LL870U6UWvQIh9w&word=' + keyWord;
+            request(url, function(err, response, data) {
+                if (!err && response.statusCode == 200) {
+                    parseString(data, function(error, parsedJson) {
+                        if (!error && parsedJson.results.result) {
+                            Synonyms = parsedJson.results.result[0].synonyms;
+                            Antonyms = parsedJson.results.result[0].antonyms;
+                            Speech = parsedJson.results.result[0].partofspeech;
+                            if (Definition == "" || Definition == null)
+                                Definition = parsedJson.results.result[0].definition;
+                        }
 
-module.exports.socket = function(socket){
-        
-        socket.on('message',function(data){
-                socket.broadcast.emit('message',data);        
-        });
+                    });
+                }
+                res.send({
+                    KeyWord: keyWord,
+                    Speech: Speech,
+                    Audio: Audio,
+                    Definition: Definition,
+                    Usage: Usage,
+                    Phrase: Phrase,
+                    Synonyms: Synonyms,
+                    Antonyms: Antonyms,
+                    Anagrams: Anagrams,
+                    Images: Images
+                });
+            });
+        }
 
-        
+        getWordDetails(inputWord);
+    });
 };
